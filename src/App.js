3612d7921227b07f6d8092d4adc536507648adc7
Alex <alex.jacquot.dev@gmail.com>
2024-02-01 15:53:55 +0100

    Updated API error handling / added metadata fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Character from './Character';
 import { Refresh } from './Refresh';
+import Metadata from './Metadata';
 
 function App() {
 
+  useEffect(() => {
+
+  }, [])
+
+
   return (<>
+    <Metadata />
     <Refresh />
     <Character /></>
   );
