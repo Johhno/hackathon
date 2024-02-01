db0616be453f6d2ff238f7b995542473d45c9778
Alex <alex.jacquot.dev@gmail.com>
2024-02-01 09:43:40 +0100

    Initial commit for react app and dotenv

--- src/index.js
+++ src/index.js
@@ -15,3 +15,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const apiKey = process.env.REACT_APP_API_KEY;
+const apiUrl = process.env.REACT_APP_API_URL;
