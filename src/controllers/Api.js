5cf946ef921762ecd3ffdecc46c9b750ed5bf8f1
Teo Aaugry <teo.augr@limayrac.fr>
2024-02-01 14:43:33 +0100

    Change substring error

--- src/controllers/Api.js
+++ src/controllers/Api.js
@@ -31,12 +31,17 @@ function fetchData() {
     headers
   )
     .then((data) => {
+      // le nombre d'offres total recupérer sur 
       let nb = data.resultats.length;
+      // offre d'emploi aleatoire
       let offre = data.resultats[parseInt(Math.random() * (nb * 1) + 1)];
+      let dateCreationOffre = new Date();
+      dateCreationOffre = offre.dateCreation;
 
+      // remplissage des meta datas qui vont servir a la creation du monstre et pour l'affichages des informations de l'offre d'emploi
       metaDatas.intitule = offre.intitule;
       metaDatas.experienceLibelle = offre.experienceLibelle;
-      metaDatas.dateCreation = offre.dateCreation.substring(0, 10);
+      metaDatas.dateCreation = dateCreationOffre.substring(0, 10);
       metaDatas.departement = offre.lieuTravail.codePostal.substring(0, 2);
       metaDatas.experienceExige = offre.experienceExige;
     })
